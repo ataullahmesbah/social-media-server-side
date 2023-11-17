eb289302f2514a46c6b3cfa40972f8ca17c17f3c
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-11-18 03:31:44 +0600

    user auth set up done and resolves issues fixeds

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
-/*  File Storage */
+/* -   File Storage  - */
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cd(null, "public/assets");
