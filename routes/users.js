058580eb6553eaa542a7f4472134763d0e65695a
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-11-18 23:35:41 +0600

    user verifation token and friends request token

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
-/* READ */
+/*  READ  */
 router.get('/:id', verifyToken, getUser);
 router.get('/:id/friends', verifyToken, getUserFriends);
 
